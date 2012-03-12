d8b2b1f53c9bb72342b02adff16ecdf5a899c9ef
dzucconi <damonzucconi@gmail.com>
2012-03-12 11:30:29 -0400

    fixed

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require 'bundler'
 Bundler.require
 
-helpers do
-  # TODO: Put helpers here.
-end
-
 module AssetHelpers
   def asset_path(source)
     "/assets/" + settings.sprockets.find_asset(source).digest_path
@@ -15,7 +11,6 @@ class App < Sinatra::Base
   set :root, File.expand_path('../', __FILE__)
   set :sprockets, Sprockets::Environment.new(root)
   set :precompile, [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ]
-  # set :precompile, [ /.*/ ]
   set :assets_prefix, 'assets'
   set :assets_path, File.join(root, 'public', assets_prefix)
   
@@ -23,17 +18,18 @@ class App < Sinatra::Base
     sprockets.append_path(File.join(root, 'assets', 'stylesheets'))
     sprockets.append_path(File.join(root, 'assets', 'javascripts'))
     sprockets.append_path(File.join(root, 'assets', 'images'))
-    
     sprockets.context_class.instance_eval do
       include AssetHelpers
     end
   end
-end
-
-get '/' do
-  haml :index
-end
-
-get '/*' do
-  haml :not_found
+  
+  helpers do
+    include AssetHelpers
+  end
+  
+  set :haml, :format => :html5
+  
+  get "/" do
+    haml :index
+  end
 end
