a1e9199d9865514f8407e887e538e2fca6432290
dzucconi <damonzucconi@gmail.com>
2012-07-24 17:25:45 -0400

    actually encrypt the values

--- app/controllers/blocks.rb
+++ app/controllers/blocks.rb
@@ -1,7 +1,9 @@
 Jtt.controllers :blocks do
 
   get :show, :map => "/view/:id" do
-    @block = Arena::Client.block(params[:id])
+    @block = Arena::Client.block(
+        params[:e] ? params[:id] : slug_decode(params[:id])
+      )
     
     render "blocks/show"
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,19 @@
 Jtt.helpers do
 
+  # Not using this ATM... but fun.
   def rot(string)
     string.tr 'A-Za-z','N-ZA-Mn-za-m'
   end
 
+  def encrypt_value(val); Encryptor.encrypt(val, :key => KEY); end
+  def decrypt_value(val); Encryptor.decrypt(val, :key => KEY); end
+
   def slug_encode(slug)
-    rot(slug)
+    Base64.urlsafe_encode64(encrypt_value(slug))
   end
 
   def slug_decode(slug)
-    rot(slug)
+    decrypt_value(Base64.urlsafe_decode64(slug))
   end
 
 end
