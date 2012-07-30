18d11dc8097ab75bc0a7e6852b7acb404825349b
dzucconi <damonzucconi@gmail.com>
2012-07-30 09:22:06 -0400

    render full size representation when appropriate

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -5,6 +5,18 @@ Jtt.helpers do
     "http://d2ss1gpcas6f9e.cloudfront.net/?resize=#{dimensions}%3E&src=#{src}"
   end
 
+  def render_full_or_thumb(size)
+    size > CHANNEL_SIZE_THRESHOLD
+  end
+
+  def render_full_or_thumb_path(size)
+    render_full_or_thumb(size) ? "thumbs" : "full"
+  end
+
+  def render_full_or_thumb_class(blocks, block)
+    render_full_or_thumb(blocks.size) ? block['block_type'] : 'full'
+  end
+
   BLOCK_TYPES.each do |type|
     define_method "block_is_#{type}?" do |block|
       block['block_type'] == type.to_s.capitalize
