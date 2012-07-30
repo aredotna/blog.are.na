f7ad237c4b255930d6a2448d180c6c6bdb10f216
dzucconi <damonzucconi@gmail.com>
2012-07-30 08:53:08 -0400

    helper methods for switching block type in template, disable area link for texts

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -4,4 +4,10 @@ Jtt.helpers do
   def image_proxy_path(src, dimensions='900x900')
     "http://d2ss1gpcas6f9e.cloudfront.net/?resize=#{dimensions}%3E&src=#{src}"
   end
+
+  BLOCK_TYPES.each do |type|
+    define_method "block_is_#{type}?" do |block|
+      block['block_type'] == type.to_s.capitalize
+    end
+  end
 end
