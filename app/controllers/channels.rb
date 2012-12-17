da0e691f3a561b605faac7c0bc3d175ac1104343
dzucconi <damonzucconi@gmail.com>
2012-12-17 12:46:08 -0500

    Clean up

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -1,19 +1,12 @@
 App.controllers :channels do
   get :index, map: "/"  do
     @channel = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 10, page: params[:page]})
-    # @channel.contents.reverse!
-    
     render 'channels/show'
   end
 
 
   get :show, map: "/:id" do
     @channel = Arena.channel slug_decode(params[:id])
-    
-    if request.xhr?
-      render 'channels/show', layout: false
-    else
-      render 'channels/show'
-    end
+    render 'channels/show'
   end
 end
