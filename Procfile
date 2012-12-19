b3775f9e7bdd90f9829fcff429ffbbed04fc3cac
dzucconi <damonzucconi@gmail.com>
2012-12-19 17:20:48 -0500

    Procfile for foreman, configure arena gem

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: padrino start -p 9999
--- app/controllers/blocks.rb
+++ app/controllers/blocks.rb
@@ -1,7 +1,7 @@
 App.controllers :blocks do
 
   get :show, map: "/view/:id" do
-    @block = Arena.block slug_decode(params[:id]
+    @block = Arena.block slug_decode(params[:id])
     
     render "blocks/show"
   end
