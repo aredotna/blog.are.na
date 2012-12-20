d74b22ba4f6945173d57db83ada485cd538d51f1
Dan Brewster <dnbrwstr@gmail.com>
2012-12-20 16:10:27 -0500

    Baseline style & structure

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -1,12 +1,18 @@
 App.controllers :channels do
+
   get :index, map: "/"  do
-    @channel = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 10, page: params[:page]})
+    @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
+    @first_post_slug = @menu.contents.first.slug
+    @post_date = @menu.contents.first.connected_at
+    @post = Arena.channel @first_post_slug
     render 'channels/show'
   end
 
-
   get :show, map: "/:id" do
-    @channel = Arena.channel slug_decode(params[:id])
+    @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
+    @post = Arena.channel slug_decode(params[:id])
+    @post_date = @menu.contents.select {|m| m.slug == @post.slug}.first.connected_at
     render 'channels/show'
   end
 end
+
