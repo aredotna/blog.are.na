2e281444b0b7676f98014aacc27d436753d2a509
Dan Brewster <dnbrwstr@gmail.com>
2013-01-02 12:20:54 -0500

    Combine routes

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -1,19 +1,15 @@
 App.controllers :channels do
 
-  get :index, map: "/"  do
+  get :index, map: "/(:id)"  do
     @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
-    @post = Arena.channel @menu.contents.first.slug
-    render 'channels/show'
-  end
-
-  get :show, map: "/:id" do
-    @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
-    if @menu.contents.map(&:slug).include? params[:id]
+    if params[:id] && @menu.contents.map(&:slug).include?(params[:id])
       @post = Arena.channel params[:id]
     else
       @post = Arena.channel @menu.contents.first.slug
     end
+    @next_post = next_block(@menu, @post)
     render 'channels/show'
   end
+
 end
 
