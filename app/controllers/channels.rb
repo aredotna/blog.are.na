53546dcd0fc99a62442d0c6e8b04a582d3797edd
Dan Brewster <dnbrwstr@gmail.com>
2013-01-02 11:46:01 -0500

    Unencoded slugs  (check against menu contents)

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -8,7 +8,11 @@ App.controllers :channels do
 
   get :show, map: "/:id" do
     @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
-    @post = Arena.channel slug_decode(params[:id])
+    if @menu.contents.map(&:slug).include? params[:id]
+      @post = Arena.channel params[:id]
+    else
+      @post = Arena.channel @menu.contents.first.slug
+    end
     render 'channels/show'
   end
 end
