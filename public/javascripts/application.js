0ffb5fd9cacee5760e4a60e533fbec82ade93f6d
Dan Brewster <dnbrwstr@gmail.com>
2013-01-02 15:22:29 -0500

    Move connections with scroll

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,50 @@ $(document).ready(function(){
 
   $('.post-container').hover(function(){
     content = $(this).find('.connections-preview-content')
-    
-    content.css({
-      // top: $(this).position().top + $('#main').scrollTop() - 10
-    })
-
-    content.show()
-
+    content.addClass('current');
   }, function(){
     content = $(this).find('.connections-preview-content')
-    content.hide()
+    content.removeClass('current')
   })
+
+  $('#main').scroll(function(){
+    $('.connections-preview-content.current').each(function(){
+      var cont = $(this).closest('.post-container')
+      if (cont.position().top > 0) {
+        $(this).css({
+          top: 0
+        })
+      } else {
+        $(this).css({
+          top: -$(cont).position().top
+        })
+      }
+    })
+  })
+
+  // $('#main').scroll(function(){
+  //   var topEl = document.elementFromPoint(300, 100)
+  //   var post = $(topEl).closest('.post-container')
+
+  //   if (post.length){
+  //     var connections = post.find('.connections-preview-content')
+  //     connections.show()
+
+  //     if (post.position().top < 20){
+  //       if ($(this).position().top)
+  //       connections.css({
+  //         position: 'absolute',
+  //         top: $('#main').scrollTop() + 20,
+  //         left: 20
+  //       })
+  //     } else {
+  //       connections.css({
+  //         position: 'absolute',
+  //         top: $('#main').scrollTop() + post.position().top,
+  //         left: 20
+  //       })
+  //     }
+  //   }
+  // })
 
 })
