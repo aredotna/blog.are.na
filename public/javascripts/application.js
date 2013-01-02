7697bc9390cf403252aba3efb8da2ddea93f2da9
Dan Brewster <dnbrwstr@gmail.com>
2013-01-02 11:38:45 -0500

    Correct connection positioning

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+
   $('.post-container').hover(function(){
     content = $(this).find('.connections-preview-content')
     
     content.css({
-      top: $(this).offset().top - 10
+      top: $(this).position().top + $('#main').scrollTop() - 10
     })
 
     content.show()
