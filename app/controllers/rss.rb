4ba2d8435e28d2bd4347c072a880fe52a75e7464
Dan Brewster <dnbrwstr@gmail.com>
2013-01-08 16:24:16 -0500

    RSS Feed

---
+++ app/controllers/rss.rb
@@ -0,0 +1,17 @@
+App.controllers :rss do
+
+  get :index, map: "/rss"  do
+    expires_in 720
+    blog_channel = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 20})
+    blog_posts = blog_channel.contents
+    @posts = blog_posts.map do |p|
+      {
+        :post_date=>p.connected_at,
+        :post_channel=>Arena.channel(p.slug, {per:20})
+      }
+    end
+    haml(:'feed/feed', :format => :xhtml, :escape_html => true, :layout => false)
+  end
+
+end
+
