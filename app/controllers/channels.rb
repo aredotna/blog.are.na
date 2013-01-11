645d6b33de5ae5392fc6eec93b291c90a97f2677
Dan Brewster <dnbrwstr@gmail.com>
2013-01-11 12:10:39 -0500

    Filter blocks

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -2,11 +2,13 @@ App.controllers :channels do
 
   get :index, map: "/(:id)", :priority => :low do
     @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
+    @menu.contents.reject! {|b| b._class != "Channel"}
     if params[:id] && @menu.contents.map(&:slug).include?(params[:id])
       @post = Arena.channel params[:id]
     else
       @post = Arena.channel @menu.contents.first.slug
     end
+    
     @next_post = next_block(@menu, @post)
     render 'channels/show'
   end
