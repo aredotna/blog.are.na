e8874c4e5f211671d44dd4b30bd63c56261a1d2d
Dan Brewster <dnbrwstr@gmail.com>
2013-01-11 11:33:40 -0500

    Set root channel in ENV

--- lib/arena_init.rb
+++ lib/arena_init.rb
@@ -1,5 +1,5 @@
 SITE_TITLE = 'Arena / Blog'
-DEFAULT_CHANNEL_IDENTIFIER = 'blog-test'
+DEFAULT_CHANNEL_IDENTIFIER = ENV['ROOT_CHANNEL']
 KEY = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 BLOCK_TYPES = [:image, :text, :link, :media, :attachment, :channel]
 
