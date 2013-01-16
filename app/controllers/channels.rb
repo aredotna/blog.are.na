9b3908c86cd77220cc29ae782284e9c986abf2f4
Dan Brewster <dnbrwstr@gmail.com>
2013-01-16 12:08:14 -0500

    Padrino sometimes gives id as array?

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -3,8 +3,10 @@ App.controllers :channels do
   get :index, map: "/(:id)", :priority => :low do
     @menu = Arena.channel(DEFAULT_CHANNEL_IDENTIFIER, { sort: 'created_at', direction: 'desc', per: 30, page: params[:page]})
     @menu.contents.reject! {|b| b._class != "Channel"}
-    if params[:id] && @menu.contents.map(&:slug).include?(params[:id])
-      @post = Arena.channel params[:id]
+    
+    id = params[:id].class == Array ? params[:id].pop : params[:id]
+    if id && @menu.contents.map(&:slug).include?(id)
+      @post = Arena.channel id
     else
       @post = Arena.channel @menu.contents.first.slug
     end
@@ -12,6 +14,5 @@ App.controllers :channels do
     @next_post = next_block(@menu, @post)
     render 'channels/show'
   end
-
 end 
 
